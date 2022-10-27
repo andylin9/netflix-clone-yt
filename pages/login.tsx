87f2a856688d2f05a3c8dd0f649380e89808f74e
andygithub9 <108387233+andylin9@users.noreply.github.com>
2022-10-27 13:25:24 +0800

    day 2 use local image

--- pages/login.tsx
+++ pages/login.tsx
@@ -41,7 +41,7 @@ function Login() {
       />
 
       <img
-        src="https://rb.gy/ulxxee"
+        src="/Netflix_2015_logo.svg"
         className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
         width={150}
         height={150}
